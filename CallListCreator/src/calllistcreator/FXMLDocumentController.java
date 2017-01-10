8fadb48283d642926d589ec67dc755054c9a00e7
Spectram6 <gdm415@lionmail.lindenwood.edu>
2017-01-09 23:04:24 -0600

    Add files via upload

---
+++ CallListCreator/src/calllistcreator/FXMLDocumentController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calllistcreator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class FXMLDocumentController {
+
+    @FXML
+    private HBox hBox;
+
+    @FXML
+    private VBox bigVBox;
+    
+    @FXML
+    private VBox smallVBox;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    void createButton(ActionEvent event) {
+        
+        
+    }
+    
+    @FXML
+    void addAPerson(ActionEvent event) {
+        
+        
+        
+    }
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
